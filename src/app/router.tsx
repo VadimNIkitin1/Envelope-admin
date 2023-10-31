611a94a7f99e72cabb27d4b037df40f7e0fad448
VadimNikitin1 <vchivas18years@gmail.com>
2023-10-31 15:45:50 +0300

    fix

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,20 +1,20 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
-// import ErrorPage from '../pages/ErrorPage/ErrorPage';
-// import AboutPage from '../pages/AboutPage/AboutPage';
-// import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
-// import AuthPage from '../pages/AuthPage/AuthPage';
-// import Layout from '../widgets/Layout/Layout';
-// import LoginPage from '../pages/LoginPage/LoginPage';
-// import PricesPage from '../pages/PricesPage/PricesPage';
-// import MenuPage from '../pages/MenuPage/MenuPage';
-// import ClientsPage from '../pages/ClientsPage/ClientsPage';
-// import NotificationPage from '../pages/NotificationPage/NotificationPage';
-// import AnalyticPage from '../pages/AnalyticPage/AnalyticPage';
-// import SettingsPage from '../pages/SettingsPage/SettingsPage';
-// import RequireAuth from '../features/HOC/RequireAuth';
-// import StoreCardsPage from '../pages/StoreCardsPage/StoreCardsPage';
-// import CategoriesPage from '../pages/CategoriesPage/CategoriesPage';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import AboutPage from '../pages/AboutPage/AboutPage';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import AuthPage from '../pages/AuthPage/AuthPage';
+import Layout from '../widgets/Layout/Layout';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import PricesPage from '../pages/PricesPage/PricesPage';
+import MenuPage from '../pages/MenuPage/MenuPage';
+import ClientsPage from '../pages/ClientsPage/ClientsPage';
+import NotificationPage from '../pages/NotificationPage/NotificationPage';
+import AnalyticPage from '../pages/AnalyticPage/AnalyticPage';
+import SettingsPage from '../pages/SettingsPage/SettingsPage';
+import RequireAuth from '../features/HOC/RequireAuth';
+import StoreCardsPage from '../pages/StoreCardsPage/StoreCardsPage';
+import CategoriesPage from '../pages/CategoriesPage/CategoriesPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
