a06323782f4c98f6576635e14cf4844687c5a114
VadimNikitin1 <vchivas18years@gmail.com>
2023-11-10 17:08:15 +0300

    fix

--- src/entities/StoreCard/StoreCard.tsx
+++ src/entities/StoreCard/StoreCard.tsx
@@ -3,11 +3,11 @@ import { useAppSelector } from '../../types/hooks';
 import { clsx } from 'clsx';
 import style from './StoreCard.module.scss';
 
-const StoreCard = ({ name }) => {
+const StoreCard = (props) => {
   const theme = useAppSelector((state) => state.active.theme);
   return (
     <div className={clsx(style.card, theme && style.light)}>
-      <p>{name}</p>
+      <p>{props.name}</p>
     </div>
   );
 };
