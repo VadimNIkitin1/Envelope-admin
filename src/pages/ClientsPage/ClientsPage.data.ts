56a14f8e9f7cc6edefa26b65dcdac74c045efa8b
VadimNikitin1 <vchivas18years@gmail.com>
2023-11-28 19:41:34 +0300

    fix

--- src/pages/ClientsPage/ClientsPage.data.ts
+++ src/pages/ClientsPage/ClientsPage.data.ts
@@ -3,7 +3,6 @@ export const TABLE_HEADER_CLIENTS = [
   { name: 'User ID', code: 'tg_user_id', type: 'str' },
   { name: 'Имя', code: 'first_name', type: 'str' },
   { name: 'Фамилия', code: 'last_name', type: 'str' },
-  { name: 'Телефон', code: 'phone', type: 'str' },
   { name: 'Сумма заказов', code: 'total_sales', type: 'str' },
   { name: 'Последний заказ', code: 'last_order_date', type: 'str' },
   { name: 'Премиум', code: 'is_premium', type: 'bool' },
