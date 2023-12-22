fd5a29a7bafa4244b702400c427783c131640800
VadimNikitin1 <vchivas18years@gmail.com>
2023-12-22 16:13:15 +0300

    fix

--- src/widgets/SideBar/SideBar.tsx
+++ src/widgets/SideBar/SideBar.tsx
@@ -4,15 +4,17 @@ import { clsx } from 'clsx';
 import SideBarList from '../../entities/SideBarList/SideBarList';
 
 import { useAppSelector } from '../../types/hooks';
+import { useLocation } from 'react-router';
+import { PATHNAME } from '../../app/constants';
 
 const SideBar = () => {
+  const location = useLocation();
   const theme = useAppSelector((state) => state.active.theme);
 
-  return (
+  return location.pathname.includes(PATHNAME.STORES) ? null : (
     <section className={clsx(style.sidebar, theme && style.light)}>
       <SideBarList />
     </section>
   );
 };
-
 export default SideBar;
