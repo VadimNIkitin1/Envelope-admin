81cd267e79a726094d20e4ca3e7ba2f2cf8de6d3
VadimNikitin1 <vchivas18years@gmail.com>
2024-01-09 14:30:35 +0300

    Task#0 - add: Добавлен пробная настройка ci/cd

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="vite/client" />
+
+// interface ImportMeta {
+//   readonly hot: {
+//     accept(callback: (mod: any) => void): void;
+//   };
+// }
