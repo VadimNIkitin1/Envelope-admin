07700c3bbec48ea3e0342de6458bcc912aa1f76d
VadimNikitin1 <vchivas18years@gmail.com>
2024-01-09 14:30:52 +0300

    fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,1 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="vite/client" />
-
-// interface ImportMeta {
-//   readonly hot: {
-//     accept(callback: (mod: any) => void): void;
-//   };
-// }
